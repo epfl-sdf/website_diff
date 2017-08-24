60a801d07df26e871c5cf24609b0ebabec54bf7a
Christian Zufferey <christian@zufferey.com>
2017-08-24 11:26:11 +0200

    - ajouté le -y dans le script install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ dataFold="../data_web_diff"
 screenFold="copy_screen"
 
 sudo apt-get update
-sudo apt-get install python3-dev python3-pip
-sudo apt-get install xvfb
-sudo apt-get install firefox
-sudo apt install virtualenv
+sudo apt-get -y install python3-dev python3-pip
+sudo apt-get -y install xvfb
+sudo apt-get -y install firefox
+sudo apt-get -y install virtualenv
 
 wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
 tar xf geckodriver-v0.18.0-linux64.tar.gz
